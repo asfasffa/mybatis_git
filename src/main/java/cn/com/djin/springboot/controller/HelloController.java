d7ce8f43f170d5852d3ff6df4a5162f1b0626425
邵枫 <1228236741@qq.com>
2021-06-08 17:58:50 +0800

    第3次上传

---
+++ src/main/java/cn/com/djin/springboot/controller/HelloController.java
@@ -0,0 +1,18 @@
+package cn.com.djin.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 邵枫
+ * @date 2021/6/8 17:56
+ */
+@Controller
+@RequestMapping("/index")
+public class HelloController {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
+}
